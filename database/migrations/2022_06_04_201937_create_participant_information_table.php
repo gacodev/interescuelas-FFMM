89f919f77e11daddbfc07f6970f13bd2d36a0ea3
“Gabriel <gabriel.contrerasv3@gmail.com>
2022-06-03 16:30:22 -0500

    agregando estructura base de datos interescuelas

---
+++ database/migrations/2022_06_04_201937_create_participant_information_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('number_id')->unique();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->date('birthday');
+            $table->foreignId('doc_id')->constrained();
+            $table->foreignId('force_id')->constrained();
+            $table->foreignId('nationality_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+};
