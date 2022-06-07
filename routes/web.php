d5021ee5905bc0e3760e08b36694a8312d772760
jrcerquera <jacerquera021@misena.edu.co>
2022-06-06 23:13:01 -0500

    ajustando driver

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Storage;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/', function () {
+    /*
+    return view('welcome');
+    */
+    return Storage::disk('public_site')->get('index.html');
 });
