d6ac0732ca504c2832659b84c541df55b4eec100
“Gabriel <gabriel.contrerasv3@gmail.com>
2022-06-13 00:32:03 -0500

    agregando el footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,5 +79,11 @@
             @yield('content')
         </main>
     </div>
+    <footer class="bg-light text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2022 Copyright:
+          <a class="text-dark" href="https://fac.mil.co">Fuerza Aerea Colombiana</a>
+        </div>
+      </footer>
 </body>
 </html>
