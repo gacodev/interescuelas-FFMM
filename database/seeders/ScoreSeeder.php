fb9d07441f8ffbaba52fee9a73de99dfa0415a06
“Gabriel <gabriel.contrerasv3@gmail.com>
2022-06-12 23:16:48 -0500

    agregando informacion dumy y relacion de la fuerza segun el usuario

--- database/seeders/ScoreSeeder.php
+++ database/seeders/ScoreSeeder.php
@@ -20,6 +20,12 @@ class ScoreSeeder extends Seeder
             ['competence_id' => 1, 'participant_id' => 2, 'award_id' => 2],
             ['competence_id' => 1, 'participant_id' => 3, 'award_id' => 3],
             ['competence_id' => 2, 'participant_id' => 4, 'award_id' => 1],
+            ['competence_id' => 2, 'participant_id' => 5, 'award_id' => 2],
+            ['competence_id' => 2, 'participant_id' => 6, 'award_id' => 3],
+            ['competence_id' => 3, 'participant_id' => 7, 'award_id' => 1],
+            ['competence_id' => 3, 'participant_id' => 8, 'award_id' => 2],
+            ['competence_id' => 3, 'participant_id' => 9, 'award_id' => 3],
+            ['competence_id' => 4, 'participant_id' => 10, 'award_id' => 1],
         ]);
 
     }
