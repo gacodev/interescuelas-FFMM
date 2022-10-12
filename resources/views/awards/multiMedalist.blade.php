ddfa1fab0cc25e4caab49b0d8b15fc3ad67b96db
Gabriel Contreras <gabriel.contrerasv3@gmail.com>
2022-10-12 00:13:44 -0500

    multimedallistas

--- resources/views/awards/multiMedalist.blade.php
+++ resources/views/awards/multiMedalist.blade.php
@@ -8,64 +8,69 @@
 
         </div>
 
-
-        <div>
+        <div class="row">
             @foreach ($participants as $participant)
-                <div class="card border-dark my-1 d-inline-block ">
-                    <div class="card-header text-center text-white"
-                        style="background-color:{{ $participant->force->color }}">
-                        <strong>{{ $participant->force->force }}</strong>
-                        <img src="{{ $participant->force->force_image }}" width="50" height="50" alt=""
-                            class="d-inline">
-                    </div>
-                    <img class="img-card" src="{{ $participant->photo }}" alt="" width="150" height="100">
-                    <div class="card-body text-dark">
-                        <div>
-                            <img src="{{ $participant->nationality->flag_image }}" width="50" height="50"
-                                alt="" class="d-inline">
+                <div class="col-12 col-md-6 col-lg-4 p-2">
+                    <div class="card border-dark">
+                        <div class="card-header text-center text-white"
+                            style="background-color:{{ $participant->force->color }}">
+                            <strong>{{ $participant->force->force }}</strong>
+                            <img src="{{ $participant->force->force_image }}" width="50" height="50" alt=""
+                                class="d-inline">
                         </div>
-                        <div class="">
-                            <div class="mb-2 text-left">
-                                <p class="card-text lh-1 mt-2"><strong>Nacionalidad: </strong>
-                                    {{ $participant->nationality->nationality }}</p>
-                                <p class="card-text lh-1"><strong>Nombre: </strong>{{ $participant->name }}
-                                </p>
-                                <p class="card-text lh-1"><strong>Edad:
-                                    </strong>{{ $participant->birthday }}</p>
-                                <p class="card-text lh-1"><strong> Genero:
-                                    </strong>{{ $participant->sexo }}</p>
-                            </div>
-                            @role('admin')
-                                <div class="text-center">
-                                    <button type="button" class="m-1 btn btn-primary" data-bs-toggle="modal"
-                                        data-bs-target="#ver{{ $participant->id }}">Ver</button>
-                                </div>
-                            @endrole
-                        </div>
-                    </div>
-                </div>
-
-
-
-                <div class="modal" tabindex="-1" id="ver{{ $participant->id }}" role="dialog">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title"></h5>
-                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
+                        <div class="card-body text-dark">
+                            <div>
                                 <div>
+                                    <img class="img-card d-inline-block img-fluid img-thumbnail"
+                                        src="{{ $participant->photo }}" alt="">
 
-                                    aqui ira el contenido del perfil del participante
+                                    <img src="{{ $participant->nationality->flag_image }}" width="80" height="80"
+                                        style="position: absolute; right: 1.5rem; " alt=""
+                                        class="mt-2 d-inline-block">
+
                                 </div>
                             </div>
-                            <div class="modal-footer">
+                            <div class="">
+                                <div class="mb-2 text-left">
+                                    <p class="card-text lh-1 mt-2"><strong>Nacionalidad: </strong>
+                                        {{ $participant->nationality->nationality }}</p>
+                                    <p class="card-text lh-1"><strong>Nombre: </strong>{{ $participant->name }}
+                                    </p>
+                                    <p class="card-text lh-1"><strong>Medallas: </strong>
+                                    </p>
+                                </div>
+                                @role('admin')
+                                    <div class="text-center">
+                                        <button type="button" class="m-1 btn btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#ver{{ $participant->id }}">Ver</button>
+                                    </div>
+                                @endrole
+                            </div>
+                        </div>
+                    </div>
 
-                                <button type="button" id="cerrar" class="btn btn-danger"
-                                    data-bs-dismiss="modal">Cerrar</button>
+
+
+                    <div class="modal" tabindex="-1" id="ver{{ $participant->id }}" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title"></h5>
+                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div>
+
+                                        aqui ira el contenido del perfil del participante
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+
+                                    <button type="button" id="cerrar" class="btn btn-danger"
+                                        data-bs-dismiss="modal">Cerrar</button>
+                                </div>
                             </div>
                         </div>
                     </div>
