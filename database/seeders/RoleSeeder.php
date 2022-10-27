53370cf1f0d1cb518085370862403ed0fa1482d8
Gabriel Contreras <gabriel.contrerasv3@gmail.com>
2022-10-27 08:46:11 -0500

    roles y permisos de los usuario

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,39 +19,39 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $role1 = Role::create(['name' => 'admin']);
-        $role2 = Role::create(['name' => 'consumer']);
+        $role2 = Role::create(['name' => 'customer']);
         $role3 = Role::create(['name' => 'viewer']);
         $permission = Permission::create(['name' => 'participants.create'])->syncRoles('admin');
-        $permission = Permission::create(['name' => '/participantes/mostrar'])->syncRoles('admin','consumer','viewer');
-        $permission = Permission::create(['name' => '/participantes/editar'])->syncRoles('admin','consumer');
+        $permission = Permission::create(['name' => '/participantes/mostrar'])->syncRoles('admin','customer','viewer');
+        $permission = Permission::create(['name' => '/participantes/editar'])->syncRoles('admin','customer');
         $permission = Permission::create(['name' => '/participantes/registro'])->syncRoles('admin');
-        $permission = Permission::create(['name' => '/participantes/index'])->syncRoles('admin','consumer');
+        $permission = Permission::create(['name' => '/participantes/index'])->syncRoles('admin','customer');
 
 
-        $permission = Permission::create(['name' => '/staff/create'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/staff/show'])->syncRoles(['admin','consumer','viewer']);
-        $permission = Permission::create(['name' => '/staff/edit'])->syncRoles(['admin','consumer']);
+        $permission = Permission::create(['name' => '/staff/create'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/staff/show'])->syncRoles(['admin','customer','viewer']);
+        $permission = Permission::create(['name' => '/staff/edit'])->syncRoles(['admin','customer']);
         $permission = Permission::create(['name' => '/staff/destroy'])->syncRoles(['admin']);
-        $permission = Permission::create(['name' => '/staff/index'])->syncRoles(['admin','consumer','viewer']);
+        $permission = Permission::create(['name' => '/staff/index'])->syncRoles(['admin','customer','viewer']);
 
 
-        $permission = Permission::create(['name' => '/awards/create'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/awards/show'])->syncRoles(['admin','consumer','viewer']);
-        $permission = Permission::create(['name' => '/awards/edit'])->syncRoles(['admin','consumer']);
+        $permission = Permission::create(['name' => '/awards/create'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/awards/show'])->syncRoles(['admin','customer','viewer']);
+        $permission = Permission::create(['name' => '/awards/edit'])->syncRoles(['admin','customer']);
         $permission = Permission::create(['name' => '/awards/destroy'])->syncRoles(['admin']);
-        $permission = Permission::create(['name' => '/awards/index'])->syncRoles(['admin','consumer','viewer']);
+        $permission = Permission::create(['name' => '/awards/index'])->syncRoles(['admin','customer','viewer']);
 
-        $permission = Permission::create(['name' => '/roles/create'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/roles/show'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/roles/edit'])->syncRoles(['admin','consumer']);
+        $permission = Permission::create(['name' => '/roles/create'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/roles/show'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/roles/edit'])->syncRoles(['admin','customer']);
         $permission = Permission::create(['name' => '/roles/destroy'])->syncRoles(['admin']);
-        $permission = Permission::create(['name' => '/roles/index'])->syncRoles(['admin','consumer']);
+        $permission = Permission::create(['name' => '/roles/index'])->syncRoles(['admin','customer']);
 
 
-        $permission = Permission::create(['name' => '/disciplinas/index'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/disciplinas/create'])->syncRoles(['admin','consumer']);
-        $permission = Permission::create(['name' => '/disciplinas/edit'])->syncRoles(['admin','consumer']);
+        $permission = Permission::create(['name' => '/disciplinas/index'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/disciplinas/create'])->syncRoles(['admin','customer']);
+        $permission = Permission::create(['name' => '/disciplinas/edit'])->syncRoles(['admin','customer']);
         $permission = Permission::create(['name' => '/disciplinas/destroy'])->syncRoles(['admin']);
-        
+
     }
 }
